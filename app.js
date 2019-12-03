8fed7d70ebf908a64b563e20201c658dc95400ea
Faqahat <Faqahat@gmx.com>
2019-12-04 02:36:32 +0500

    For Square Matrics

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let sizeX = 4;
-let sizeY = 4;
+let size = 4;
 let arr = [
     [3 , 9 , 2 , 5],
     [0 , 1 , 0 , 6],
@@ -11,22 +10,22 @@ let arr = [
  let highest = {
      sum : 0
  };
- for(let x = 0; x < sizeX  ; x++) 
+ for(let x = 0; x < size  ; x++) 
  {
-    for(let y = 0; y < sizeY  ; y++) 
+    for(let y = 0; y < size  ; y++) 
     {
         let sum;
-        if(x + 1  <  sizeX && x  <  sizeX &&  y  <  sizeY && y + 1  <  sizeY)
+        if(x + 1  <  size && x  <  size &&  y  <  size && y + 1  <  size)
         {
             sum = arr[x][y]  + arr[x+1][y] + arr[x][y+1] + arr[x+1][y+1];
-            if(highest.sum < sum) 
+            if(highest.sum < sum)
             highest = { a : x  , b : x+1 , c : y , d : y+1 , sum : sum }
 
         }
     }
 }
 console.log(
-
+    
     "(", highest.a , "," , highest.c , ")" , 
     "(", highest.b , "," , highest.c , ")",
     "(", highest.a , "," , highest.d , ")",
