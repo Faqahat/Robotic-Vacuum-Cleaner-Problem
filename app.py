3203082cdc8b327d77c0c553402a93aa72defb7e
Faqahat <Faqahat@gmx.com>
2019-12-04 23:24:57 +0500

    Optimized

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 import numpy as np
-s = '445621207591863034'
-s = '445621997591863034'
+s = '445699209991863034'
+#s = '445621997591863034'
 s = np.array(list(s), dtype=np.uint8)
 x = s[2:].reshape(s[0],-1)
 size = s[0]
-highest = 0
-cords = {}
+cords = {
+    "sum" : 0
+}
 for i in range(0, len(s)):
     if i > 0:
         for j in range(0, len(s)):
             sum = 0
             if i + 1  <  size and i  <  size and  j  <  size and j + 1  <  size:
                 sum = x[i][j]  + x[i+1][j] + x[i][j+1] + x[i+1][j+1]
-                if sum > highest:
+                if sum > cords["sum"]:
                     highest = sum
                     cords = {
                         "a" : i,
                         "b" : i+1,
                         "c" : j,
-                        "d" : j+1
+                        "d" : j+1,
+                        "sum" : sum
                     }
-print "(", cords["b"] , "," , cords["c"], ")", "(", cords["d"] , "," , cords["b"] , ") ","(", cords["d"] , "," , cords["a"] , ")" ,"(", cords["c"] , "," , cords["a"] , ")" , highest
+print "(", cords["c"] , "," , cords["b"], ")", "(", cords["d"] , "," , cords["b"] , ") ","(", cords["d"] , "," , cords["a"] , ")" ,"(", cords["c"] , "," , cords["a"] , ")" , cords["sum"]
 
  
                 
